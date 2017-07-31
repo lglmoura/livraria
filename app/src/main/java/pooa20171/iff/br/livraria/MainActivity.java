24d06cc0a96f970e7ed2fa90b3275735dacc2130
Luiz Gustavo <lglmoura@gmail.com>
2017-07-31 19:32:57 -0300

    Inicio do Projeto

---
+++ app/src/main/java/pooa20171/iff/br/livraria/MainActivity.java
@@ -0,0 +1,13 @@
+package pooa20171.iff.br.livraria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
